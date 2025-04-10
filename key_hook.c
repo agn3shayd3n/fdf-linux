0e7a998fd8d2bc68ce79d77eac3f2e1ccc76f82c
Agnes <agn3shayd3n@users.noreply.github.com>
2025-04-10 16:39:28 +0200

    almost done

--- key_hook.c
+++ key_hook.c
@@ -29,13 +29,13 @@ static int	handle_arrows(int keycode, void *param)
 	t_fdf	*fdf;
 
 	fdf = (t_fdf *)param;
-	if (keycode == 65363)
+	if (keycode == 65361)
 		fdf->view.offset_x -= 30;
-	else if (keycode == 65361)
+	else if (keycode == 65363)
 		fdf->view.offset_x += 30;
-	else if (keycode == 65362)
-		fdf->view.offset_y += 30;
 	else if (keycode == 65364)
+		fdf->view.offset_y += 30;
+	else if (keycode == 65362)
 		fdf->view.offset_y -= 30;
 	return (0);
 }
@@ -45,14 +45,14 @@ static int	handle_axis(int keycode, void *param)
 	t_fdf	*fdf;
 
 	fdf = (t_fdf *)param;
-	if (keycode == 113)
+	if (keycode == 101)
 		fdf->view.angle_x += 0.1;
-	else if (keycode == 101)
+	else if (keycode == 113)
 		fdf->view.angle_x -= 0.1;
 	else if (keycode == 119)
 		fdf->view.angle_y += 0.1;
 	else if (keycode == 115)
-		fdf->view.angle_y -= 0.1;
+		fdf->view.angle_y -= 0.1; // i have to fix this
 	return (0);
 }
 
@@ -77,6 +77,7 @@ int	handle_key(int keycode, void *param)
 	t_fdf	*fdf;
 
 	fdf = (t_fdf *)param;
+	printf("%d\n", keycode);
 	if (keycode == 65307)
 		exit(0);
 	else if (keycode == 122 || keycode == 120)
